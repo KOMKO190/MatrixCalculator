08de44b3c1652985dcb966160d655d803b932882
KOMKO190 <55060846+KOMKO190@users.noreply.github.com>
2023-11-27 22:46:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+require('./node_modules/matrix-js', '1.6.1')
+
 function testing() {
     console.log("Hello, World!");
-    console.log("Made by Luka Bedic")
+    var a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    var A = matrix(a);
+    A();
+    A(0);
 }
 testing();
--- script.ts
+++ script.ts
@@ -1,5 +1,12 @@
+import * as Matrix from './node_modules/';
+
 function testing() {
     console.log("Hello, World!");
+
+    var a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+    var A = Matrix.matrix(a);
+    A();
+    A(0);
 }
 
 testing();
